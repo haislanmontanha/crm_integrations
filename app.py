a16b0c7ab8d93706c570a6441fedd2f209e7f181
haislanmontanha <haislan.nascimento@gmail.com>
2022-02-02 12:00:57 +0000

    Opçoes de menu integraçao NectarCrm

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ import os
 import requests
 import json
 from flask import Flask, request, jsonify
+from datetime import datetime
+
 
 app = Flask(__name__)
 
 api_contact = 'https://app.nectarcrm.com.br/crm/api/1/contatos/'
 api_oportunidades = 'https://app.nectarcrm.com.br/crm/api/1/oportunidades/'
-api_local= "https://5000-haislanmontanha-gev-55t1r8kq5qw.ws-us29.gitpod.io/"
+api_qualificacoes = 'https://app.nectarcrm.com.br/crm/api/1/qualificacoes/'
+api_local= "https://5000-haislanmontanha-gev-55t1r8kq5qw.ws-us29.gitpod.io/1643277681253-convertido.pdf"
 menu_cpf = "cpf"
 menu_cnpj = "cnpj"
 menu_telefone = "telefone"
@@ -92,16 +95,6 @@ def menu_user(user_json, msg):
         "items":[
             {
                 "number":1,
-                "text":"Analisar Estatistica",
-                "callback":{
-                    "endpoint": api_local+"nectarcrm_statistics",
-                    "data":{
-                        "user": user_json
-                    }
-                }
-            },
-            {
-                "number":2,
                 "text":"Próxima tarefa",
                 "callback":{
                     "endpoint": api_local+"nectarcrm_proximaAtividade",
@@ -109,27 +102,27 @@ def menu_user(user_json, msg):
                         "user": user_json
                     }
                 }
-            },
-            {
-                "number":3,
-                "text":"Qualificações",
-                "callback":{
-                    "endpoint": api_local+"nectarcrm_qualificacao",
-                    "data":{
-                        "user": user_json
-                    }
-                }
-            },
-            {
-                "number":4,
-                "text":"Oportunidade",
-                "callback":{
-                    "endpoint": api_local+"nectarcrm_oportunidade",
-                    "data":{
-                        "user": user_json
-                    }
-                }
             }
+            # {
+            #     "number":2,
+            #     "text":"Qualificações",
+            #     "callback":{
+            #         "endpoint": api_local+"nectarcrm_qualificacao",
+            #         "data":{
+            #             "user": user_json
+            #         }
+            #     }
+            # },
+            # {
+            #     "number":3,
+            #     "text":"Oportunidade",
+            #     "callback":{
+            #         "endpoint": api_local+"nectarcrm_oportunidade",
+            #         "data":{
+            #             "user": user_json
+            #         }
+            #     }
+            # }
         ]
     }
 
@@ -149,6 +142,26 @@ def response_question(text, callback):
                 
             }
         }
+    }
+
+def response_information(text, urldoc):
+    return {
+        "type":"INFORMATION",
+        "text":text,
+        "attachments":[
+            {
+                "position":"BEFORE",
+                "type":"IMAGE",
+                "name":"image.png",
+                "url":"https://itsstecnologia.com.br/blogs/wp-content/uploads/2021/04/integracao-na-empresa.png"
+            },
+            {
+                "position":"AFTER",
+                "type":"DOCUMENT",
+                "name":"document.pdf",
+                "url":"http://www.africau.edu/images/default/sample.pdf"
+            }
+        ]
     }
 
 def informacao_invalida(msg_menu):
@@ -196,6 +209,10 @@ def getUser(request_mz, msg_menu):
                 
     elif (request_mz.status_code == 404):
         return {"error": "Request must be JSON"}, 404
+
+@app.get("/")
+def start():
+    return "Hello World!", 201
 
 @app.post("/nectarcrm")
 def start_nectarcrm():
@@ -264,6 +281,189 @@ def getemail_nectarcrm():
 @app.post("/nectarcrm_oportunidade")
 def getoportunidade_nectarcrm():
     if request.is_json:
-        return {'msg':'Chegou aqui - nectarcrm_oportunidade'}, 201
 
+        mz = request.get_json()
+        text = mz["text"]
+        data = mz["data"]
+        data_size = len(data)
+
+        print(f"Content: {data} Size: {data_size}")
+
+        if data_size==0:
+            return menu_inicial("Olá, não foi possivel encontrar nenhuma oportunidade, por favor informe uma das seguintes informações.")
+        elif data["user"]["id"]:
+
+            user_id = data["user"]["id"]
+            user_cpf = data["user"]["cpf"]
+
+            print(f"User id: {user_id} User CPF: {user_cpf}")
+
+            request_mz = requests.get(api_oportunidades+'contatoId/'+str(user_id), params=params, headers=headers)
+
+            print(f"Status Code: {request_mz.status_code}, Url: {request_mz.url}")
+
+            if (request_mz.status_code == 200):
+                    print("The request was a success!")
+                    # Code here will only run if the request is successful
+                    resposta_json = request_mz.json()
+                    json_size = len(resposta_json)
+
+                    print(f"Status Code: {request_mz.status_code}, Content: {resposta_json}, Size Json {json_size}")
+
+                    if json_size == 0:
+                        return informacao_invalida(menu_proximaAtividade), 201
+                    else:
+                        
+                        return response_information("", ""), 201
+                            
+            elif (request_mz.status_code == 404):
+                return {"error": "Request must be JSON"}, 404
+            # return {'msg':'Chegou aqui - nectarcrm_oportunidade'}, 201
+
+    return {"error": "Request must be JSON"}, 415
+
+@app.post("/nectarcrm_proximaAtividade")
+def getproximaAtividade_nectarcrm():
+    if request.is_json:
+
+        mz = request.get_json()
+        text = mz["text"]
+        data = mz["data"]
+        data_size = len(data)
+
+        print(f"Content: {data} Size: {data_size}")
+
+        if data_size==0:
+            return menu_inicial("Olá, não foi possivel encontrar nenhuma atividade, por favor informe uma das seguintes informações.")
+        elif data["user"]["id"]:
+
+            user_id = data["user"]["id"]
+            user_cpf = data["user"]["cpf"]
+
+            print(f"User id: {user_id} User CPF: {user_cpf}")
+
+            request_mz = requests.get(api_contact+str(user_id)+'/proximaAtividade', params=params, headers=headers)
+
+            print(f"Status Code: {request_mz.status_code}, Url: {request_mz.url}")
+
+            if (request_mz.status_code == 200):
+                    print("The request was a success!")
+                    # Code here will only run if the request is successful
+                    resposta_json = request_mz.json()
+                    json_size = len(resposta_json)
+
+                    print(f"Status Code: {request_mz.status_code}, Content: {resposta_json}, Size Json {json_size}")
+
+                    if json_size == 0:
+                        return informacao_invalida(menu_proximaAtividade), 201
+                    else:
+                        titulo = resposta_json["titulo"]
+                        descricao = resposta_json["descricao"]
+                        responsavel = resposta_json["responsavel"]["nome"]
+                        cliente = resposta_json["cliente"]["nome"]
+                        tarefa = resposta_json["tarefaTipo"]["nome"]
+                        dataLimite = resposta_json["dataLimite"]
+                        dataCriacao = resposta_json["dataCriacao"]
+
+                        f = "%Y-%m-%dT%H:%M:%S.%fZ"
+                        dataLimite_f = datetime.strptime(dataLimite, f)
+
+                        dataCriacao_f = datetime.strptime(dataLimite, f)
+
+                        msg_information = f"Titulo: {titulo},\n Descrição: {descricao},\n Responsavel: {responsavel},\n Cliente: {cliente},\n Tarefa: {tarefa},\n Criada em: {dataCriacao_f},\n Limite de entrega: {dataLimite_f}"
+
+                        return response_information(msg_information, ""), 201
+                            
+            elif (request_mz.status_code == 404):
+                return {"error": "Request must be JSON"}, 404
+
+            # return {'msg':'Chegou aqui - nectarcrm_proximaAtividade'}, 201
+
+    return {"error": "Request must be JSON"}, 415
+
+
+@app.post("/nectarcrm_statistics")
+def getstatistics_nectarcrm():
+    if request.is_json:
+
+        mz = request.get_json()
+        text = mz["text"]
+        data = mz["data"]
+        data_size = len(data)
+
+        print(f"Content: {data} Size: {data_size}")
+
+        if data_size==0:
+            return menu_inicial("Olá, não foi possivel encontrar nenhuma statistica, por favor informe uma das seguintes informações.")
+        elif data["user"]["id"]:
+
+            user_id = data["user"]["id"]
+            user_cpf = data["user"]["cpf"]
+
+            print(f"User id: {user_id} User CPF: {user_cpf}")
+
+            request_mz = requests.get(api_contact+'statistics/'+str(user_id), params=params, headers=headers)
+
+            print(f"Status Code: {request_mz.status_code}, Url: {request_mz.url}")
+
+            if (request_mz.status_code == 200):
+                    print("The request was a success!")
+                    # Code here will only run if the request is successful
+                    resposta_json = request_mz.json()
+                    json_size = len(resposta_json)
+
+                    print(f"Status Code: {request_mz.status_code}, Content: {resposta_json}, Size Json {json_size}")
+
+                    if json_size == 0:
+                        return informacao_invalida(menu_statistics), 201
+                    else:
+                       
+                        return response_information("", ""), 201
+                            
+            elif (request_mz.status_code == 404):
+                return {"error": "Request must be JSON"}, 404
+            # return {'msg':'Chegou aqui - nectarcrm_statistics'}, 201
+
+    return {"error": "Request must be JSON"}, 415
+
+@app.post("/nectarcrm_qualificacao")
+def getqualificacao_nectarcrm():
+    if request.is_json:
+
+        mz = request.get_json()
+        text = mz["text"]
+        data = mz["data"]
+        data_size = len(data)
+
+        print(f"Content: {data} Size: {data_size}")
+
+        if data_size==0:
+            return menu_inicial("Olá, não foi possivel encontrar nenhuma qualificação, por favor informe uma das seguintes informações.")
+        elif data["user"]["id"]:
+
+            user_id = data["user"]["id"]
+            user_cpf = data["user"]["cpf"]
+
+            print(f"User id: {user_id} User CPF: {user_cpf}")
+
+            request_mz = requests.get(api_qualificacoes+'contatoId/'+str(user_id), params=params, headers=headers)
+
+            print(f"Status Code: {request_mz.status_code}, Url: {request_mz.url}")
+
+            if (request_mz.status_code == 200):
+                    print("The request was a success!")
+                    # Code here will only run if the request is successful
+                    resposta_json = request_mz.json()
+                    json_size = len(resposta_json)
+
+                    print(f"Status Code: {request_mz.status_code}, Content: {resposta_json}, Size Json {json_size}")
+
+                    if json_size == 0:
+                        return informacao_invalida(menu_proximaAtividade), 201
+                    else:
+                       
+                        return response_information("", ""), 201
+                            
+            elif (request_mz.status_code == 404):
+                return {"error": "Request must be JSON"}, 404
     return {"error": "Request must be JSON"}, 415
