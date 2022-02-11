0fdfb997ccfb1d6514a1db9643dbb802b166cb3e
Gilson Montanha <gilson.montnaha@solvis.com.br>
2022-02-11 10:34:45 -0300

    Server production

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@ from app import app
 from os import environ
 
 if __name__ == "__main__":
-    SERVER_HOST = environ.get("SERVER_HOST", "https://crmintegrations.herokuapp.com/")
+    SERVER_HOST = environ.get("SERVER_HOST", "localhost")
     app.run(
         host=SERVER_HOST,
         port=5500,
