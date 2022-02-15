f603009d0fb5ffbf33235539cb6166ef21db4590
Gilson <98980237+gjmontanha@users.noreply.github.com>
2022-02-15 08:57:22 -0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     app.run(
         host='0.0.0.0',
         port=port,
-        debug=(not environ.get("ENV") == "PRODUCTION"),
-        threaded=True,
+        #debug=(not environ.get("ENV") == "PRODUCTION"),
+        #threaded=True,
     )
