e466d8e6748e9860e4192c7035f15b64866d70be
Gilson <98980237+gjmontanha@users.noreply.github.com>
2022-02-15 08:41:56 -0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,10 +2,12 @@ from app import app
 from os import environ
 
 if __name__ == "__main__":
-    SERVER_HOST = environ.get("SERVER_HOST", "localhost")
+    #SERVER_HOST = environ.get("SERVER_HOST", "localhost")
+    port = int(os.environ.get("PORT", 5500))
+    app.run(host='0.0.0.0', port=port)
     app.run(
-        host=SERVER_HOST,
-        port=5500,
+        host='0.0.0.0',
+        port=port,
         debug=(not environ.get("ENV") == "PRODUCTION"),
         threaded=True,
     )
