1e92ac798f51192707db4fa217cff19a43b195ff
Gilson <98980237+gjmontanha@users.noreply.github.com>
2022-02-16 09:56:01 -0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,10 +2,9 @@ from app import app
 from os import environ
 
 if __name__ == "__main__":
-    SERVER_HOST = environ.get("SERVER_HOST", "localhost")
+    app.debug = True
     app.run(
-        host=SERVER_HOST,
+        host="crmintegrations.herokuapp.com",
         port=5500,
-        debug=(not environ.get("ENV") == "PRODUCTION"),
-        threaded=True,
+        threaded=True, 
     )
