792ac8698589ada5e51fa5ecb90213653be1d39b
Gilson Montanha <gilson.montnaha@solvis.com.br>
2022-02-16 09:39:00 -0300

    Implementando procura de atividade no HubSpot

--- wsgi.py
+++ wsgi.py
@@ -2,4 +2,10 @@ import app
 import Flask
 
 if __name__ == "__main__":
-    app.run(host='crmintegrations.herokuap.com' ,port=5000, debug = True)
+    SERVER_HOST = environ.get("SERVER_HOST", "localhost")
+    app.run(
+        host=SERVER_HOST,
+        port=5500,
+        debug=(not environ.get("ENV") == "PRODUCTION"),
+        threaded=True,
+    )
