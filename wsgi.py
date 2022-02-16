a35e2f51b2084cb83ee2fc4fba1fc169d64ce2b6
Gilson <98980237+gjmontanha@users.noreply.github.com>
2022-02-16 10:08:50 -0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,8 +3,4 @@ from os import environ
 
 if __name__ == "__main__":
     app.debug = True
-    app.run(
-        host="crmintegrations.herokuapp.com",
-        port=5500,
-        threaded=True, 
-    )
+    app.run()
