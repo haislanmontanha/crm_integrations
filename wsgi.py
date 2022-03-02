9f40284ae3addc94d146d949a49410f6738a8910
Gilson J. Montanha Jr <gilson.montanha@echo.com>
2022-03-02 07:43:05 -0300

    testing build on port 5005

--- wsgi.py
+++ wsgi.py
@@ -2,10 +2,10 @@ from app import app
 from os import environ
 
 if __name__ == "__main__":
-    SERVER_HOST = environ.get("SERVER_HOST", "localhost")
+    SERVER_HOST = environ.get("SERVER_HOST", "0.0.0.0")
     app.run(
         host=SERVER_HOST,
-        port=5000,
+        port=5005,
         debug=(not environ.get("ENV") == "PRODUCTION"),
         threaded=True,
     )
